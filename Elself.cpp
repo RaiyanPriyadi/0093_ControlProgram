0b588f37298dff3da04af3b43e8ef17da9489daf
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-03-14 03:13:20 +0700

    Refactor Elself.cpp for clarity and structure

--- Elself.cpp
+++ Elself.cpp
@@ -20,7 +20,7 @@ int main() {
 
     cout << "Masukkan Nilai Bahasa Inggris = ";
     cin >> nilBI;
-    cout << "Masukkan Nili Matematika = ";
+    cout << "Masukkan Nilai Matematika = ";
     cin >> nilMT;
 
    //rerata = (nilBI+nilMT)/2;
